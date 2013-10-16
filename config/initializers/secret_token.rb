e4e6130aded09f9b89c03c80377d17ee0675d7ee
pvin <praaveenranganathan@gmail.com>
2013-10-16 23:11:24 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dbrelation::Application.config.secret_token = 'f09b1a61d563b8f5f9747cbb9ad8983337212a89b91a8289708deaecdc243f79da0050b662937b862a6304a85138010adfa5d55d42fcfca1d7f0430c0dd5b52a'
